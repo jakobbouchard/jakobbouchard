7dff23cbcbd99fc81d396797b3866b6ee3bbc505
Jakob Bouchard <jakob@jakobbouchard.dev>
2022-12-21 13:53:36 -0500

    Remove postcss, adapter-static

--- postcss.config.cjs
+++
@@ -1,7 +0,0 @@
-const postcssPresetEnv = require('postcss-preset-env');
-const cssnano = require('cssnano');
-
-/** @type {import("postcss-load-config").Config} */
-module.exports = {
-	plugins: [postcssPresetEnv({ stage: 1 }), cssnano({ preset: 'default' })]
-};
