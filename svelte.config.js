4156b8897f133866eb966997721c550136800e96
Jakob Bouchard <jakob@jakobbouchard.dev>
2022-12-24 16:05:48 -0500

    fuck what i said, vercel is cooler.

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,7 +7,7 @@ const config = {
 	// for more information about preprocessors
 	preprocess: [preprocess()],
 
-	kit: { adapter: adapter() }
+	kit: { adapter: adapter({ split: true }) }
 };
 
 export default config;
