71cc8d32bb0098bc2442ad6ba77d820623b90706
Jakob Bouchard <jakob@jakobbouchard.dev>
2023-02-09 16:53:54 -0500

    okay well isr didn't work, whatever

--- src/routes/(app)/projects/[slug]/+page.server.ts
+++ src/routes/(app)/projects/[slug]/+page.server.ts
@@ -2,14 +2,6 @@ import { error } from "@sveltejs/kit";
 import { getClient } from "$lib/sanity/client";
 import { projectQuery, type Project } from "$lib/sanity/queries";
 import type { PageServerLoad } from "./$types";
-import { ISR_BYPASS_TOKEN } from "$env/static/private";
-
-export const config = {
-	isr: {
-		expiration: 60,
-		bypassToken: ISR_BYPASS_TOKEN,
-	},
-};
 
 export const load = (async ({ params: { slug }, parent }) => {
 	const { isPreview, isEmbedPreview } = await parent();
