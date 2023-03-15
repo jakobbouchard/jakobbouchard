eaae0ff2dcb42f86051692144dc833f1b195df4c
Jakob Bouchard <jakob@jakobbouchard.dev>
2023-03-15 19:24:30 -0400

    Add way to feature projects

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,7 @@ export const collections = {
 	work: defineCollection({
 		schema: z.object({
 			title: z.string(),
+			featured: z.boolean().optional(),
 			description: z.string(),
 			publishDate: z.coerce.date(),
 			tags: z.array(z.string()),
--- src/content/work/bloom-box.md
+++ src/content/work/bloom-box.md
@@ -1,5 +1,6 @@
 ---
 title: Bloom Box
+featured: true
 publishDate: 2019-12-01 00:00:00
 img: /assets/stock-2.jpg
 img_alt: A bright pink sheet of paper used to wrap flowers curves in front of rich blue background
