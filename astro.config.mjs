02af783ceef4f718c464e37b2b2ad3d7780b0386
Jakob Bouchard <jakob@jakobbouchard.dev>
2023-03-15 22:45:53 -0400

    Change page title, add card hover effect

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,17 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://jakobbouchard.dev",
-  assets: "_app",
-  experimental: {
-    assets: true
-  },
-  server: {
-    port: 5173,
-    host: true
-  },
-  image: {
-    service: "astro/assets/services/sharp"
-  },
-  integrations: [sitemap()]
+	site: "https://jakobbouchard.dev",
+	assets: "_app",
+	experimental: {
+		assets: true,
+	},
+	server: {
+		port: 5173,
+		host: true,
+	},
+	image: {
+		service: "astro/assets/services/sharp",
+	},
+	integrations: [sitemap()],
 });
