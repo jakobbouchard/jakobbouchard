ede1f29ef6fb3287b54a04ffbfa3eb59a4269cd9
Jakob Bouchard <jakob@jakobbouchard.dev>
2023-05-19 03:28:03 -0400

    Fix build

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,7 +9,7 @@ export const collections = {
 				attribution: z.string().optional(),
 				publishDate: z.coerce.date(),
 				tags: z.array(z.string()),
-				videoPreview: z.string(),
+				videoPreview: z.string().optional(),
 				img: image().optional(),
 				imgAlt: z.string().optional(),
 				url: z.string().optional(),
